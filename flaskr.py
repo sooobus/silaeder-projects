af19259964fbca0f4955acbfde8512ca5c0944f2
Andreyk <all.andrey@bk.ru>
2018-12-11 15:49:23 +0300

    commit10

--- flaskr.py
+++ flaskr.py
@@ -1,7 +1,7 @@
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
-#lol
+#lolkek
 # configuration
 DATABASE = 'projects.db'
 #DEBUG = True
