adda519b5fa5fa05c118006b05e9e94dcdd7d8a6
Даша <dashulya-1997+DashaVysotskaya@github.com>
2019-02-05 11:59:10 +0300

    cjcjc

--- flaskr.py
+++ flaskr.py
@@ -1,7 +1,6 @@
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
-#test cooment
 # configuration
 DATABASE = 'projects.db'
 #DEBUG = True
